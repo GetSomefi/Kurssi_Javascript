b7a2da6bce9264be0dbb77248b49f87d929e621d
Peter@MSI_GP66 <Peter.virtanen@samk.fi>
2024-10-15 13:23:09 +0300

    regex muutos

--- haeTeksti(RegEx)/esim.js
+++ haeTeksti(RegEx)/esim.js
@@ -1,31 +1,46 @@
-//test()
+//test() 
+//tutkii löytyykö merkki merkkijonosta
+//vastaus true tai false
 const tutkittavaTeksti = "Onkohan tässä mitään järkeä tai kärkeä";     //tutkittava teksti
-let hae = /ä/; //vain ensimmäinen
+let hae = /ä/; //vain ensimmäinen   (eli jo yksi "ä" tuottaa true) 
 console.log( "1. ", hae.test(tutkittavaTeksti) );
 
 //match()
+//tutkii merkkijonosta yhtäläisyyksiä säännölliseen lausekkeeseen (reqular expression)
+//jos löytyy palauttaa array
+//muuten null
 hae = /ärkeä/g; // <- käytetään mukana g-lippua jotta saadaan kaikki osumat. Ilman tulostuu vain ensimmäinen osuma
 console.log( "2. ", tutkittavaTeksti.match(hae) );
 
 //includes()
+//tutkii löytyykö merkki merkkijonosta. Huomio pienen ja ison kirjaimen (case sensitive).
+//vastaus true tai false
 console.log( "3. ", tutkittavaTeksti.includes("ä") );
 
 //indexOf()
+//tutkii löytyykö merkki merkkijonosta. Huomio pienen ja ison kirjaimen (case sensitive).
+//jos löytyy palauttaa löydetyn merkin sijainnin
+//muuten -1 
 console.log( "4. ", tutkittavaTeksti.indexOf("ä") );
                                             
-// Tulostaa:
-// 1.  true                                 //test() palauttaa vain tiedon löytyikö. Jos merkkiä ei löytynyt saadaan arvoksi false.
-// 2.  [ 'ärkeä', 'ärkeä' ]                 //match() palauttaa kaikki osumat. Jos merkkiä ei löytynyt saadaan arvoksi null.
-// 3.  true                                 //includes() palauttaa vain tiedon löytyikö. Jos merkkiä ei löytynyt saadaan arvoksi false.
-// 4.  9                                    //indexOf() palauttaa sijainnin mistä merkki löytyi. Jos merkkiä ei löytynyt saadaan arvoksi -1.  
-
 //search()
+//tutkii merkkijonosta yhtäläisyyksiä säännölliseen lausekkeeseen (reqular expression)
+//jos löytyy palauttaa löydetyn merkin sijainnin
+//muuten -1 
 console.log(  "5. ", tutkittavaTeksti.search("ä")  );
 
 let salasana = "TosiHeikko";
 if(salasana.search(/[0-9]/) < 1){
     console.log("6. Salasanan tulee sisältää vähintään yksi numero");
 }
+
+// Tulostaa:
+// 1.  true                                 //test() palauttaa vain tiedon löytyikö. Jos merkkiä ei löytynyt saadaan arvoksi false.
+// 2.  [ 'ärkeä', 'ärkeä' ]                 //match() palauttaa kaikki osumat. Jos merkkiä ei löytynyt saadaan arvoksi null.
+// 3.  true                                 //includes() palauttaa vain tiedon löytyikö. Jos merkkiä ei löytynyt saadaan arvoksi false.
+// 4.  9                                    //indexOf() palauttaa sijainnin mistä merkki löytyi. Jos merkkiä ei löytynyt saadaan arvoksi -1.  
+//5.  9                                     //search() palauttaa sijainnin mistä merkki löytyi. Jos merkkiä ei löytynyt saadaan arvoksi -1.
+//6. Salasanan tulee sisältää vähintään yksi numero
 
 /* 
 Yleisimpiä säännöllisten lausekkeiden (RegEx) osia
