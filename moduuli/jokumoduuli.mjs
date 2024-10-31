5065be73baca811d53b8c62b5d7107a26824ca7d
Peter@MSI_GP66 <Peter.virtanen@samk.fi>
2024-10-31 18:28:48 +0200

    bulk

--- moduuli/jokumoduuli.mjs
+++ moduuli/jokumoduuli.mjs
@@ -1,3 +1,7 @@
 export function summa (luku1,luku2){
     return luku1+luku2;
 }
+
+export function siisti (siistittavaTeksti) {
+    return siistittavaTeksti.replaceAll(/[ .#@äö]/g,"");
+}
--- moduuli/moduulinkayttaja.mjs
+++ moduuli/moduulinkayttaja.mjs
@@ -1,5 +1,8 @@
 //https://exploringjs.com/js/book/ch_syntax.html#modules
 
-import {summa} from './jokumoduuli.mjs';
+// !! Huomaa, että tiedostopääte on .mjs ei .js
 
+import {summa, siisti} from './jokumoduuli.mjs';
 console.log("Summa", summa(1,2));
+
+console.log("Siisti", siisti("#Hästäg@mope di.fi"));
